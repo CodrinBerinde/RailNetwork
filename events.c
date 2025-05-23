36dcdcab1ad5a262eab67cd74b68874f1e4ef3bd
Codrin Berinde <berincod@fit.cvut.cz>
2025-05-23 17:27:10 +0300

    move up down test06

--- events.c
+++ events.c
@@ -32,6 +32,7 @@ void init_reading_constants() {
 }
 
 int listen_event() {
+  int res = 0;
   uint32_t knobs = *(uint32_t *)(spiled_base + SPILED_REG_KNOBS_8BIT_o);
   uint32_t knob_red = (knobs >> 16) & 255;
   uint32_t knob_green = (knobs >> 8) & 255;
@@ -40,18 +41,18 @@ int listen_event() {
   if(knob_red != prev_knob_red) {
     if(knob_red > prev_knob_red) {
       if(knob_red - prev_knob_red < SAFETY_KNOB_DIFFERENCE) //it means that the knob moved up
-        return 2;
+        res = 2;
       else        //the knob moved down
-        return 4;
+        res = 4;
     } else {
       if(prev_knob_red - knob_red < SAFETY_KNOB_DIFFERENCE) //it means that the knob moved down
-        return 4;
+        res = 4;
       else
-        return 2;
+        res = 2;
     }
     prev_knob_red = knob_red;
   }
 
   //printf("I read: red=%d, green=%d, blue=%d.\n", (knobs >> 16) & 255, (knobs >> 8) & 255, knobs & 255);
-  return 0;
+  return res;
 }

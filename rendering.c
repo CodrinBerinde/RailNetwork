bcd4aa7982f4cbdf6139a98d10f8dc3e3d009663
Codrin Berinde <berincod@fit.cvut.cz>
2025-05-28 18:59:01 +0300

    testing the logic02

--- rendering.c
+++ rendering.c
@@ -66,7 +66,7 @@ void show_menu(int menu_row, buf_t *buf) {
   int i, j;
   for(int k = 0; k < 4; k++) {
     j = (PARLCD_WIDTH - (string_width(menu_font, menu_options[k], 2))) / 2;
-    i = MENU_CORNER_I + k * MENU_ROW_HEIGHT + (MENU_ROW_HEIGHT - MENU_FONT_SIZE * menu_font->height) / 2;
+    i = MENU_CORNER_I + k * MENU_ROW_HEIGHT + (MENU_ROW_HEIGHT - MENU_TEXT_SIZE * menu_font->height) / 2;
     str_buf(buf, i, j, menu_font, menu_options[k], MENU_TEXT_SIZE, MENU_TEXT_COLOR);
   }
 }
